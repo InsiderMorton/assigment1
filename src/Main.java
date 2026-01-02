a2fb05cdd377d6c00244c3cdb567023496665e01
InsiderMorton <insidermarlow@gmail.com>
2026-01-03 05:28:15 +0600

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-
-    public static void main(String[] args) {
-        Library_app magaLibrary=new Library_app();
-        magaLibrary.run();
-    }
-}
